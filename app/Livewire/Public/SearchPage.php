8012d759ddd43c941477dca474e789bc37d5ad27
Fajartale <fajarnich09@gmail.com>
2025-12-23 18:10:49 +0700

    test

--- app/Livewire/Public/SearchPage.php
+++ app/Livewire/Public/SearchPage.php
@@ -3,11 +3,33 @@
 namespace App\Livewire\Public;
 
 use Livewire\Component;
+use App\Models\Book;
+use Livewire\WithPagination;
+use Livewire\Attributes\Url;
 
 class SearchPage extends Component
 {
+    use WithPagination;
+
+    #[Url(as: 'q')] 
+    public $search = '';
+
     public function render()
     {
-        return view('livewire.public.search-page');
+        $books = Book::with('user')
+            ->when($this->search, function($query) {
+                $query->where('title', 'like', '%' . $this->search . '%')
+                      ->orWhere('synopsis', 'like', '%' . $this->search . '%')
+                      ->orWhereHas('user', function($q) {
+                          $q->where('name', 'like', '%' . $this->search . '%');
+                      });
+            })
+            ->latest()
+            ->paginate(12); 
+
+        return view('livewire.public.search-page', [
+            'books'  => $books,
+            'search' => $this->search, // <--- PERBAIKAN: Kirim variabel search ke view
+        ])->layout('layouts.public');
     }
 }
