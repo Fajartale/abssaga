63e12d956261200111ca49850501a96f2aa6b16b
Fajartale <fajarnich09@gmail.com>
2025-12-24 12:02:22 +0700

    test

---
+++ app/Livewire/Public/RankingPage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Public;
+
+use Livewire\Component;
+
+class RankingPage extends Component
+{
+    public function render()
+    {
+        return view('livewire.public.ranking-page');
+    }
+}
