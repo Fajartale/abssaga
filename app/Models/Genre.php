c9b5288a1b95a0526f5cd735573a896ee2996f03
Fajartale <fajarnich09@gmail.com>
2026-01-11 18:24:22 +0700

    test

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Genre extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name', 'slug'];
+
+    // Relasi: Satu genre punya banyak buku
+    public function books()
+    {
+        return $this->belongsToMany(Book::class, 'book_genre');
+    }
 }
